3ea8ec603d5fe0105f819323a6921a9f96bbf705
Roman Kovtunov <ro.kovtunov@mail.ru>
2023-06-13 21:46:45 +0300

    First commit to branch_Roma

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("Изменения сделанные в первом комите");
     }
 }
