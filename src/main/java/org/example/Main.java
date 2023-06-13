4033eac4d44d6f3b78aa1a15a9a53ef42e50f041
Roman Kovtunov <ro.kovtunov@mail.ru>
2023-06-13 21:53:04 +0300

    Second commit to branch_Roma

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Изменения сделанные в первом комите");
+        System.out.println("Вторые изменения для второго коммита");
     }
 }
